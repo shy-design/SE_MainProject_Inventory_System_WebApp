7275106228d33cd5cbaddf21860539bfc61e3826
An Doan <phuandoan1012@gmail.com>
2021-03-20 17:07:44 -0700

    Add/edit/delete feature (using database)

--- toys_inventory/toys_inventory/src/main/java/com/example/toys_inventory/ToyController.java
+++ toys_inventory/toys_inventory/src/main/java/com/example/toys_inventory/ToyController.java
@@ -76,6 +76,7 @@ public class ToyController {
 
         // processing Add Item button
         if (action.equals("add") && category.equals("game")){
+<<<<<<< Updated upstream
             Game game = new Game(toy.getId(), toy.getBrand(), toy.getName(), toy.getQtyStart(), toy.getQtySold(),
                     toy.getUnitPrice());
             gameList.add(game);
@@ -85,6 +86,21 @@ public class ToyController {
             toyList.add(toy);
             model.addAttribute("toys", toyList);
             return "table";
+=======
+            jdbcTemplate.update("INSERT INTO GAMES(BRAND, NAME, QTYSTART, QTYSOLD, UNITPRICE) VALUES(?,?,?,?,?);",
+                    toy.getBrand(), toy.getName(), toy.getQtyStart(), toy.getQtySold(), toy.getUnitPrice());
+            loadData();
+            model.addAttribute("toys", gameList);
+            return "table";
+
+        } else if (action.equals("add") && category.equals("toy")){
+            jdbcTemplate.update("INSERT INTO TOYS(BRAND, NAME, QTYSTART, QTYSOLD, UNITPRICE) VALUES(?,?,?,?,?);",
+                    toy.getBrand(), toy.getName(), toy.getQtyStart(), toy.getQtySold(), toy.getUnitPrice());
+            loadData();
+            model.addAttribute("toys", toyList);
+            return "table";
+
+>>>>>>> Stashed changes
         } else return "add";
     }
 
@@ -95,11 +111,16 @@ public class ToyController {
                               @RequestParam("id") int id,
                               @ModelAttribute Toy toy){
 
+<<<<<<< Updated upstream
         boolean validID = false;
+=======
+        boolean validID;
+>>>>>>> Stashed changes
         model.addAttribute("category", category);
 
         // processing Update Item button
         if (action.equals("update") && category.equals("game")){
+<<<<<<< Updated upstream
             Game game1 = new Game(toy.getId(), toy.getBrand(), toy.getName(), toy.getQtyStart(), toy.getQtySold(), toy.getUnitPrice());
             for (Game game: gameList) {
                 if (game.getId() == id){
@@ -114,11 +135,29 @@ public class ToyController {
                     toyList.set(toyList.indexOf(toy1),toy);
                 }
             }
+=======
+            String sqlUpdateItem = "UPDATE games\n" +
+                    "SET brand = ?, name = ?, qtyStart = ?, qtySold = ?, unitPrice = ?\n" +
+                    "WHERE id = ?;";
+            int result = jdbcTemplate.update(sqlUpdateItem, toy.getBrand(), toy.getName(), toy.getQtyStart(),
+                    toy.getQtySold(), toy.getUnitPrice(), id);
+            loadData();
+            model.addAttribute("toys", gameList);
+            return "table";
+        } else if (action.equals("update") && category.equals("toy")){
+            String sqlUpdateItem = "UPDATE toys\n" +
+                    "SET brand = ?, name = ?, qtyStart = ?, qtySold = ?, unitPrice = ?\n" +
+                    "WHERE id = ?;";
+            int result = jdbcTemplate.update(sqlUpdateItem, toy.getBrand(), toy.getName(), toy.getQtyStart(),
+                    toy.getQtySold(), toy.getUnitPrice(), id);
+            loadData();
+>>>>>>> Stashed changes
             model.addAttribute("toys", toyList);
             return "table";
         }
 
         if (action.equals("delete_item")){
+<<<<<<< Updated upstream
             if (category.equals("game")){
                 if (!gameList.removeIf(n -> (n.getId() == id))){
                     model.addAttribute("error_delete", "No item has ID: " + id + ". Please try again.");
@@ -131,12 +170,30 @@ public class ToyController {
                 model.addAttribute("toys", toyList);
             }
 
+=======
+            int result = 0;
+            if (category.equals("game")){
+                String sqlDeleteItem = "DELETE FROM games WHERE id=?;";
+                result = jdbcTemplate.update(sqlDeleteItem, id);
+                loadData();
+                model.addAttribute("toys", gameList);
+            } else if (category.equals("toy")){
+                String sqlDeleteItem = "DELETE FROM toys WHERE id=?;";
+                result = jdbcTemplate.update(sqlDeleteItem, id);
+                loadData();
+                model.addAttribute("toys", toyList);
+            }
+            if (result == 0){
+                model.addAttribute("error_delete", "No item has ID: " + id + ". Please try again.");
+            }
+>>>>>>> Stashed changes
             return "table";
         }
 
         if (action.equals("update_item")){
             validID = false;
             if (category.equals("game")){
+<<<<<<< Updated upstream
                 for (Game game: gameList) {
                     if (game.getId() == id){
                         model.addAttribute("toy", game);
@@ -149,6 +206,24 @@ public class ToyController {
                         model.addAttribute("toy", toy_updated);
                         validID = true;
                     }
+=======
+                String sqlPickGame = "SELECT * FROM games WHERE ID = ?";
+                try {
+                    Game game = jdbcTemplate.queryForObject(sqlPickGame, new BeanPropertyRowMapper<>(Game.class), id);
+                    validID = true;
+                    model.addAttribute("toy", game);
+                } catch (Exception e){
+
+                }
+            } else if (category.equals("toy")){
+                String sqlPickToy = "SELECT * FROM toys WHERE ID = ?";
+                try {
+                    Toy toy1 = jdbcTemplate.queryForObject(sqlPickToy, new BeanPropertyRowMapper<>(Toy.class), id);
+                    validID = true;
+                    model.addAttribute("toy", toy1);
+                } catch (Exception e){
+
+>>>>>>> Stashed changes
                 }
             }
 
